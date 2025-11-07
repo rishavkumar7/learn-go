a0324d94bff0675d22222d719471d2c69788066c
rishavkumar7 <89144767+rishavkumar7@users.noreply.github.com>
2025-11-07 16:31:17 +0000

    added constants file

---
+++ basics/constants.go
@@ -0,0 +1,17 @@
+package main
+
+const GRAVITY float64 = 9.81
+const SPEED_OF_LIGHT = 299792458
+
+func main() {
+	const SECRET string = "secret key"
+	const PI = 3.14
+
+	const (
+		MAX_USERS           = 1000
+		MIN_PASSWORD_LENGTH = 8
+		DEFAULT_TIMEOUT     = 30
+	)
+}
+
+// Gopher cannot be used to initialise constants. Its only used to initialise variables.
